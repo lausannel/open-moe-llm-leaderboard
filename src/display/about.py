e24dcd145bbd41d4401ebc972441fee8782046b2
Your Name <you@example.com>
2024-04-15 23:08:35 +0000

    simple fix of columns

--- src/display/about.py
+++ src/display/about.py
@@ -1,13 +1,10 @@
 from src.display.utils import ModelType
 
-TITLE = """<h1 align="center" id="space-title">MOE LLM GPU-Poor Leaderboard</h1>"""
+TITLE = """<h1 align="center" id="space-title">OPEN-MOE-LLM-LEADERBOARD</h1>"""
 
 INTRODUCTION_TEXT = """
-📐 The MOE LLM GPU-Poor Leaderboard aims to evaluate LLMs.
-
-
+The OPEN-MOE-LLM-LEADERBOARD is specifically designed to assess the performance and efficiency of various Mixture of Experts (MoE) Large Language Models (LLMs). This initiative, driven by the open-source community, aims to comprehensively evaluate these advanced MoE LLMs. We extend our gratitude to the Huggingface for the GPU community grant that supported the initial debugging process, and to [NetMind.AI](https://netmind.ai/home) for their generous GPU donation, which ensures the continuous operation of the Leaderboard.
 """
-
 LLM_BENCHMARKS_TEXT = f"""
 
 """
